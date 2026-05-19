76c77a752e45757ee779f3a037d101a123c88383
Emily <emily.quelal.gonzalez@udla.edu.ec>
2026-05-19 11:54:03 -0500

    Agregar libreria.h y tabla general

--- main.c
+++ main.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
+#include "calificaciones.h"
 
 int main() 
 {
-    
+    /* Matriz de estudiantes (arreglo de structs) */
     Estudiante lista[MAX_ESTUDIANTES];
     int total, i;
+
     printf("  SISTEMA DE CALIFICACIONES\n");
 
-     /* 1. Pedir cuantos estudiantes hay */
+    /* 1. Pedir cuantos estudiantes hay */
     total = pedirNumeroEstudiantes();
 
     /* 2. Leer los datos de cada estudiante */
     printf("\n-- Ingreso de datos --\n");
     for (i = 0; i < total; i++) {
         leerEstudiante(&lista[i], i + 1);
+    }
 
+    /* 3. Mostrar todos los resultados */
+    mostrarTablaGeneral(lista, total);
 }
-}
-
